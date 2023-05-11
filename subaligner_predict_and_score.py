5991250daf799cbbfa11817fc9fd3612edda9940
Alex Beltran <alexbeltran@berkeley.edu>
2023-05-11 16:09:10 -0700

    Attempt staging file in shared volume

--- subaligner_predict_and_score.py
+++ subaligner_predict_and_score.py
@@ -26,6 +26,11 @@ volume_names = ["movies", "movies-4k", "tv"]
 volume_mounts = [k8s.V1VolumeMount(name=x, mount_path="/" + x, sub_path=None, read_only=True) for x in volume_names]
 volumes = [k8s.V1Volume(name=x, host_path=k8s.V1HostPathVolumeSource(path="/" + x)) for x in volume_names]
 
+persistent_volumes = [k8s.V1Volume(name="shared-media",
+                                   persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="shared-media"))]
+
+persistent_volume_mounts = [k8s.V1VolumeMount(name="shared-media", mount_path="/shared", sub_path=None, read_only=False)]
+
 # instantiate the DAG
 with DAG(
     start_date=datetime(2023, 5, 3),
@@ -33,6 +38,36 @@ with DAG(
     schedule=None,
     dag_id="Align_and_Score_New_Media",
 ) as dag:
+    stage_file = KubernetesPodOperator(
+        affinity=affinity,
+        # unique id of the task within the DAG
+        task_id="stage_file",
+        # the Docker image to launch
+        image="alpine",
+        # launch the Pod on the same cluster as Airflow is running on
+        in_cluster=True,
+        # launch the Pod in the same namespace as Airflow is running in
+        namespace=namespace,
+        volumes=volumes + persistent_volumes,
+        volume_mounts=volume_mounts + persistent_volume_mounts,
+        # Pod configuration
+        # name the Pod
+        name="stage_file",
+        cmds=["cp",
+              """{{ dag_run.conf['MEDIA_PATH'] }}""",
+              "/shared/"],
+        # give the Pod name a random suffix, ensure uniqueness in the namespace
+        random_name_suffix=True,
+        # reattach to worker instead of creating a new Pod on worker failure
+        reattach_on_restart=True,
+        # delete Pod after the task is finished
+        is_delete_operator_pod=True,
+        # get log stdout of the container as task logs
+        get_logs=True,
+        # log events in case of Pod failure
+        log_events_on_failure=True,
+        do_xcom_push=True
+    )
     inspect_file = KubernetesPodOperator(
         # unique id of the task within the DAG
         task_id="inspect_file",
@@ -42,16 +77,14 @@ with DAG(
         in_cluster=True,
         # launch the Pod in the same namespace as Airflow is running in
         namespace=namespace,
-        volumes=volumes,
-        volume_mounts=volume_mounts,
+        volumes=persistent_volumes,
+        volume_mounts=persistent_volume_mounts,
         # Pod configuration
         # name the Pod
         name="inspect_file",
-        env_vars={'MEDIA_PATH': """{{ dag_run.conf['MEDIA_PATH'] }}"""},
+        env_vars={'MEDIA_PATH': "{{ task_instance.xcom_pull(task_ids='inspect_file', key='return_value')['MEDIA_PATH'] }}"},
         # give the Pod name a random suffix, ensure uniqueness in the namespace
         random_name_suffix=True,
-        # attach labels to the Pod, can be used for grouping
-        labels={"app": "backend", "env": "dev"},
         # reattach to worker instead of creating a new Pod on worker failure
         reattach_on_restart=True,
         # delete Pod after the task is finished
@@ -71,8 +104,8 @@ with DAG(
         in_cluster=True,
         # launch the Pod in the same namespace as Airflow is running in
         namespace=namespace,
-        volumes=volumes,
-        volume_mounts=volume_mounts,
+        volumes=persistent_volumes,
+        volume_mounts=persistent_volume_mounts,
         # Pod configuration
         # name the Pod
         name="predict_and_score",
@@ -123,16 +156,16 @@ with DAG(
         log_events_on_failure=True,
         secrets=secrets,
         # pass your name as an environment var
-        env_vars={"SUBALIGNER_Accuracy": """{{ ti.xcom_pull(task_ids='predict_and_score', key='Accuracy') }}""",
-                  "SUBALIGNER_Loss": """{{ ti.xcom_pull(task_ids='predict_and_score', key='Loss') }}""",
-                  "SUBALIGNER_Shift": """{{ ti.xcom_pull(task_ids='predict_and_score', key='Shift') }}""",
-                  "SUBALIGNER_Type": """{{ ti.xcom_pull(task_ids='predict_and_score', key='Type') }}""",
-                  "SUBALIGNER_Duration": """{{ ti.xcom_pull(task_ids='predict_and_score', key='Duration') }}""",
-                  "SUBALIGNER_Extension": """{{ ti.xcom_pull(task_ids='predict_and_score', key='Extension') }}""",
-                  "SUBALIGNER_Codec": """{{ ti.xcom_pull(task_ids='predict_and_score', key='Codec') }}""",
+        env_vars={"SUBALIGNER_Accuracy": """{{ ti.xcom_pull(task_ids='predict_and_score', key='SUBALIGNER_Accuracy') }}""",
+                  "SUBALIGNER_Loss": """{{ ti.xcom_pull(task_ids='predict_and_score', key='SUBALIGNER_Loss') }}""",
+                  "SUBALIGNER_Shift": """{{ ti.xcom_pull(task_ids='predict_and_score', key='SUBALIGNER_Shift') }}""",
+                  "SUBALIGNER_Type": """{{ ti.xcom_pull(task_ids='predict_and_score', key='SUBALIGNER_Type') }}""",
+                  "SUBALIGNER_Duration": """{{ ti.xcom_pull(task_ids='predict_and_score', key='SUBALIGNER_Duration') }}""",
+                  "SUBALIGNER_Extension": """{{ ti.xcom_pull(task_ids='predict_and_score', key='SUBALIGNER_Extension') }}""",
+                  "SUBALIGNER_Codec": """{{ ti.xcom_pull(task_ids='predict_and_score', key='SUBALIGNER_Codec') }}""",
                   "MONGO_HOST": "subaligner-analytics-mongodb",
                   "DB": "data",
                   "COLLECTION": "predictions"},
         do_xcom_push=True
     )
-    inspect_file >> predict_and_score >> send_results_to_db
+    stage_file >> inspect_file >> predict_and_score >> send_results_to_db
