ed54e9f635c6dd2b04aba4e121011d8c7e832549
Alex Beltran <alexbeltran@berkeley.edu>
2023-05-11 22:11:11 -0700

    switch namepsace for db call

--- subaligner_predict_and_score.py
+++ subaligner_predict_and_score.py
@@ -146,7 +146,7 @@ with DAG(
         # launch the Pod on the same cluster as Airflow is running on
         in_cluster=True,
         # launch the Pod in the same namespace as Airflow is running in
-        namespace=namespace,
+        namespace="default",
         # Pod configuration
         # name the Pod
         name="send_results_to_db",
