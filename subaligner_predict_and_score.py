25deb12a981331c5f2bc32846b201c4aff0c5c7d
albeltra <alexbeltran@berkeley.edu>
2023-05-23 05:02:35 -0700

    remove rogue ')'

--- subaligner_predict_and_score.py
+++ subaligner_predict_and_score.py
@@ -58,7 +58,7 @@ with DAG(
         # name the Pod
         name="inspect_file",
         env_vars={"mediaFile": """"{{dag_run.conf['mediaFile']}}""",
-                  "mediaInfo": """"{{dag_run.conf['mediaInfo'])}}""", 
+                  "mediaInfo": """"{{dag_run.conf['mediaInfo']}}""",
                   "stream_index": """"{{ dag_run.conf.get('stream_index', '') }}""",
                   "audio_channel": """"{{ dag_run.conf.get('audio_channel', '') }}"""},
         # give the Pod name a random suffix, ensure uniqueness in the namespace
