b93a8f88137aa4738b1b9d160e68c6c277d31115
albeltra <alexbeltran@berkeley.edu>
2023-05-23 04:25:18 -0700

    add utf-8 ecnoding

--- subaligner_predict_and_score.py
+++ subaligner_predict_and_score.py
@@ -57,8 +57,8 @@ with DAG(
         # Pod configuration
         # name the Pod
         name="inspect_file",
-        env_vars={"mediaFile": """" {{ dag_run.conf['mediaFile'] | b64encode }}""",
-                  "mediaInfo": """" {{ dag_run.conf.get('mediaInfo') | b64encode }}""",
+        env_vars={"mediaFile": """" {{ dag_run.conf['mediaFile'].encode('utf-8') | b64encode }}""",
+                  "mediaInfo": """" {{ dag_run.conf['mediaInfo'].encode('utf-8') | b64encode }}""",
                   "stream_index": """" {{ dag_run.conf.get('stream_index', '') }}""",
                   "audio_channel": """" {{ dag_run.conf.get('audio_channel', '') }}"""},
         # give the Pod name a random suffix, ensure uniqueness in the namespace
