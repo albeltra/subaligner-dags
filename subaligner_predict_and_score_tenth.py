d6339a8060654660b6f9ca3bc4719b220315674f
albeltra <alexbeltran@berkeley.edu>
2023-08-19 02:43:21 -0700

    rename dag

--- subaligner_predict_and_score_tenth.py
+++ subaligner_predict_and_score_tenth.py
@@ -45,7 +45,7 @@ with DAG(
         start_date=datetime(2023, 5, 3),
         catchup=False,
         schedule=None,
-        dag_id="Align_and_Score_New_Media_Weighted",
+        dag_id="Align_and_Score_New_Media_Tenth",
         render_template_as_native_obj=False,
         user_defined_filters={"b64encode": b64encode},
         concurrency=8,
