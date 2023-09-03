d465aeef2bcf280ae8df18dcc9925aa003465b7c
albeltra <alexbeltran@berkeley.edu>
2023-09-02 23:44:25 -0700

    fix mismatched parentheses

--- subaligner_test_labeled_memory_search.py
+++ subaligner_test_labeled_memory_search.py
@@ -34,7 +34,7 @@ server = "192.168.10.1"
 paths = ["/mnt/user/Media/Movies", "/mnt/user/Media/Movies-4K", "/mnt/user/Media/TV"]
 path_names = ["movies", "movies-4k", "tv"]
 volume_mounts = [k8s.V1VolumeMount(name=x, mount_path="/" + x, sub_path=None) for x in path_names]
-volumes = [k8s.V1Volume(name=x, nfs=k8s.V1NFSVolumeSource(path="/" + x), server=server, read_only=True) for x in paths]
+volumes = [k8s.V1Volume(name=x, nfs=k8s.V1NFSVolumeSource(path="/" + x, server=server, read_only=True)) for x in paths]
 
 volumes += [k8s.V1Volume(name="data", nfs=k8s.V1NFSVolumeSource(path="/mnt/user/subaligner-data", server=server, read_only=False))]
 volume_mounts += [k8s.V1VolumeMount(name="data", mount_path="/data", sub_path=None)]
