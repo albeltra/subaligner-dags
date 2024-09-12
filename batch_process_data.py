633efaace97380e86dd2a147d88fbabd50f0d2d4
Alex Beltran <alexbeltran@berkeley.edu>
2024-09-12 04:59:10 -0700

    fix disk string formatting

--- batch_process_data.py
+++ batch_process_data.py
@@ -217,5 +217,5 @@ with DAG(
     )
     # queue_extract_jobs >> run_extraction.expand(arguments=[["rq", "worker", "disk" + str(x), "--with-scheduler", "--url", "redis://${REDIS_HOST}:${REDIS_PORT}"] for x in range(1, 15)]) >> generate_features.expand(arguments=[[str(x)] for x in range(1, 15)])
     run_extraction.expand(
-        arguments=[[f"rq worker disk {str(x)} --with-scheduler --url redis://redis-master:6379"]
+        arguments=[[f"rq worker disk{str(x)} --with-scheduler --url redis://redis-master:6379"]
                    for x in range(1, 15)]) >> generate_features.expand(arguments=[[str(x)] for x in range(1, 15)])
