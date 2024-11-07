a4f648683f9fe3f5fe71691926a2076e13665130
Alex Beltran <alexbeltran@berkeley.edu>
2024-11-06 22:39:39 -0800

    try adding request

--- webrequests_elt.py
+++ webrequests_elt.py
@@ -23,6 +23,8 @@ with DAG(
 ) as dag:
     @task(task_id="test_run")
     def test_run(**kwargs):
+        import requests
+        import json
         end_date = kwargs["dag_run"].execution_date
         start_date = end_date - timedelta(hours=1)
 
@@ -31,7 +33,50 @@ with DAG(
 
         start = (start_date - timedelta(days=1)).replace(tzinfo=timezone.utc, microsecond=0)
         end = end_date.replace(tzinfo=timezone.utc, microsecond=0)
-        print(start, end)
+        zone = ""
+        data = {
+            "query": """query ListFirewallEvents($zoneTag: string, $filter: FirewallEventsAdaptiveFilter_InputObject) {
+                    viewer {
+                      zones(filter: { zoneTag: $zoneTag }) {
+                        firewallEventsAdaptive(
+                          filter: $filter
+                          limit: 10000
+                          orderBy: [datetime_DESC]
+                        ) {
+                          action
+                          clientAsn
+                          clientCountryName
+                          clientRequestHTTPHost
+                          clientIP
+                          clientRequestPath
+                          clientRequestHTTPHost
+                          clientRequestScheme
+                          clientRequestQuery
+                          datetime
+                          source
+                          userAgent
+                        }
+                      }
+                    }
+                  }""",
+            "variables": {
+                "zoneTag": zone,
+                "filter": {
+                    "datetime_geq": start.isoformat().replace('+00:00', 'Z'),
+                    "datetime_leq": end.isoformat().replace('+00:00', 'Z')
+                }
+            }}
+
+        url = "https://api.cloudflare.com/client/v4/graphql/"
+        headers = {
+            "Content-Type": "application/json",
+            "X-Auth-Email": "",
+            "X-Auth-Key": "" 
+        }
+
+        response = requests.post(url, headers=headers, data=json.dumps(data)).json()
+        # ['data']['viewer']['zones'][0]['firewallEventsAdaptive']
+        print(response["data"])
         return True
 
 
